efdef5376f7e4978f8dd92cbb03e728dd4b22c71
jlawson <jlawson>
2001-11-12 07:15:02 +0000

    add the PDF documentation on the sign protocol

--- signtest2.pl
+++ signtest2.pl
@@ -4,6 +4,14 @@ use strict;
 
 
 my $sign = ezsign->new("COM1");
-$sign->SendTextSimple("foo");
+#$sign->SendTextSimple(mode => 'flash',
+#                     text => "testing moo");
+#$sign->ClearPriorityText();
+
+$sign->SynchronizeLocalTime();
+$sign->SendTextSimple("foo \x0B8 \x13");
+
+#$sign->SoftReset();
+
 exit 0;
 
