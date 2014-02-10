5990a5ef0208f34d4292dd8a37d64565f1fd5594
Jeff Lawson <jeff@bovine.net>
2014-02-10 04:49:43 +0000

    demonstrate new embedded attribute syntax. move port to constant.

--- signtest2.pl
+++ signtest2.pl
@@ -1,15 +1,23 @@
+#!/usr/bin/perl
+
 use lib '.';
 use ezsign;
 use strict;
 
 
-my $sign = ezsign->new("COM1");
+#use constant SIGNPORT => 'COM2';
+use constant SIGNPORT => '/dev/ttyUSB0';
+
+
+my $sign = ezsign->new(SIGNPORT);
+
 #$sign->SendTextSimple(mode => 'flash',
 #                     text => "testing moo");
 #$sign->ClearPriorityText();
 
 $sign->SynchronizeLocalTime();
-$sign->SendTextSimple("foo \x0B8 \x13");
+#$sign->SendTextSimple("foo \x0B8 \x13");
+$sign->SendTextSimple("moo #{CALL_DATE_8} #{CALL_TIME}");
 
 #$sign->SoftReset();
 
